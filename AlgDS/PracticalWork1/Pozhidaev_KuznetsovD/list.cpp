7520e1cba173cf71294dd24b72372b231b5b60ff
Pozhidaev Aleksey <aleksejpozhidaev2006@gmail.com>
2025-09-20 19:19:27 +0300

    Pozhidaev_KuznetsovD_ADS_PW1 структура лабораторного проекта

---
+++ AlgDS/PracticalWork1/Pozhidaev_KuznetsovD/list.cpp
@@ -0,0 +1,36 @@
+/*
+Файл со
+структурой list
+функцией вывода для list
+функцией перевода list в short int
+функцией перевода short int в list
+
++ перегрузка логических операторов для работы с list
+*/
+
+struct list{
+};
+
+void print_list(list A){
+
+}
+
+list itol(short int A){
+
+}
+
+short int ltoi(list A){
+    
+}
+
+list operator&(list A, list B){
+
+}
+
+list operator|(list A, list B){
+    
+}
+
+list operator/(list A, list B){
+    
+}
