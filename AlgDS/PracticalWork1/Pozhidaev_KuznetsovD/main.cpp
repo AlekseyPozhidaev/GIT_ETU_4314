3d85e92f5751521aeb6c5a12fa35759a92dcce27
Denis Kuznetsov <denis111.kuznetsov@yandex.ru>
2025-09-25 03:26:41 +0300

    hope it's finale

--- AlgDS/PracticalWork1/Pozhidaev_KuznetsovD/main.cpp
+++ AlgDS/PracticalWork1/Pozhidaev_KuznetsovD/main.cpp
@@ -4,29 +4,49 @@
 #include <chrono>
 
 void calc(unsigned short int A, unsigned short int B, unsigned short int C, unsigned short int D) {
-	std::cout << "HEX: ";
+	std::cout << "HEX: " << std::endl;
+	std::cout << std::hex << A << std::endl;
+	std::cout << std::hex << B << std::endl;
+	std::cout << std::hex << C << std::endl;
+	std::cout << std::hex << D << std::endl;
 	std::cout << std::hex << (A & ~B & ~(C & D)) << std::endl;
 }
 void calc(bitArray A, bitArray B, bitArray C, bitArray D) {
-	std::cout << "Bits: ";
+	std::cout << "Bits: " << std::endl;;
+	print(A);
+	print(B);
+	print(C);
+	print(D);
+
 	print(A / B / (C & D));
 }
 void calc(LinkedList A, LinkedList B, LinkedList C, LinkedList D) {
-	std::cout << "List: ";
+	std::cout << "List: " << std::endl;
+	print(&A);
+	print(&B);
+	print(&C);
+	print(&D);
 	LinkedList E = (A / B / (C & D));
 	print(&E);
 }
 void calc(charArray A, charArray B, charArray C, charArray D) {
-	std::cout << "Chars: ";
+	std::cout << "Chars: " << std::endl;
+	print(A);
+	print(B);
+	print(C);
+	print(D);
 	print(A / B / (C & D));
 }
 void correctionTest() {
+
 	unsigned short int usA, usB, usC, usD, usE;
 	bitArray bA, bB, bC, bD, bE;
 	LinkedList lA, lB, lC, lD, lE;
 	charArray cA, cB, cC, cD, cE;
-	for (int l = 0; l < 1000; l++) {
-		usA = (unsigned short int)rand(), usB = (unsigned short int)rand(), usC = (unsigned short int)rand(), usD = (unsigned short int)rand();
+	for (int l = 2; l <= 16; l += 2) {
+		std::cout << "Cardinality: " << std::dec << l << std::endl;
+		usA = (unsigned short int)(rand() % (int)pow(2, l)), usB = (unsigned short int)(rand() % (int)pow(2, l))
+			, usC = (unsigned short int)(rand() % (int)pow(2, l)), usD = (unsigned short int)(rand() % (int)pow(2, l));
 		bA = StoB(usA), bB = StoB(usB), bC = StoB(usC), bD = StoB(usD);
 		lA = itol(usA), lB = itol(usB), lC = itol(usC), lD = itol(usD);
 		cA = StoC(usA), cB = StoC(usB), cC = StoC(usC), cD = StoC(usD);
@@ -37,11 +57,11 @@ void correctionTest() {
 		std::cout << std::endl;
 	}
 }
-void testB() {
+void testB(int n) {
 	const int N = 40000;
 	bitArray R[N];
 	for (int i = 0; i < N; i++) {
-		R[i] = StoB((unsigned short int)rand());
+		R[i] = StoB((unsigned short int)(rand() % (int)pow(2, n)));
 	}
 	auto t1 = std::chrono::high_resolution_clock::now();
 	for (int i = 0; i < N / 4; i++) {
@@ -49,13 +69,13 @@ void testB() {
 	}
 	auto t2 = std::chrono::high_resolution_clock::now();
 	auto dt = std::chrono::duration_cast<std::chrono::duration<double, std::micro>>(t2 - t1).count();
-	std::cout << "Processing " << N / 4 << "  Bits sets time: " << dt << std::endl;
+	std::cout << "Processing " << "  Bits sets time: " << dt / (N / 4) << std::endl;
 }
-void testUS() {
+void testUS(int n) {
 	const int N = 40000;
 	unsigned short int R[N];
 	for (int i = 0; i < N; i++) {
-		R[i] = ((unsigned short int)rand());
+		R[i] = ((unsigned short int)(rand() % (int)pow(2, n)));
 	}
 	auto t1 = std::chrono::high_resolution_clock::now();
 	for (int i = 0; i < N / 4; i++) {
@@ -63,13 +83,13 @@ void testUS() {
 	}
 	auto t2 = std::chrono::high_resolution_clock::now();
 	auto dt = std::chrono::duration_cast<std::chrono::duration<double, std::micro>>(t2 - t1).count();
-	std::cout << "Processing " << N / 4 << "   HEX sets time: " << dt << std::endl;
+	std::cout << "Processing " << "   HEX sets time: " << dt / (N / 4) << std::endl;
 }
-void testC() {
+void testC(int n) {
 	const int N = 40000;
 	charArray R[N];
 	for (int i = 0; i < N; i++) {
-		R[i] = StoC((unsigned short int)rand());
+		R[i] = StoC((unsigned short int)(rand() % (int)pow(2, n)));
 	}
 	auto t1 = std::chrono::high_resolution_clock::now();
 	for (int i = 0; i < N / 4; i++) {
@@ -77,13 +97,13 @@ void testC() {
 	}
 	auto t2 = std::chrono::high_resolution_clock::now();
 	auto dt = std::chrono::duration_cast<std::chrono::duration<double, std::micro>>(t2 - t1).count();
-	std::cout << "Processing " << N / 4 << " Chars sets time: " << dt << std::endl;
+	std::cout << "Processing " << " Chars sets time: " << dt / (N / 4) << std::endl;
 }
-void testL() {
+void testL(int n) {
 	const int N = 40000;
 	LinkedList R[N];
 	for (int i = 0; i < N; i++) {
-		R[i] = itol((unsigned short int)rand());
+		R[i] = itol((unsigned short int)(rand() % (int)pow(2, n)));
 	}
 	auto t1 = std::chrono::high_resolution_clock::now();
 	for (int i = 0; i < N / 4; i++) {
@@ -91,14 +111,21 @@ void testL() {
 	}
 	auto t2 = std::chrono::high_resolution_clock::now();
 	auto dt = std::chrono::duration_cast<std::chrono::duration<double, std::micro>>(t2 - t1).count();
-	std::cout << "Processing " << N / 4 << " Lists sets time: " << dt << std::endl;
+	std::cout << "Processing " << " Lists sets time: " << dt / (N / 4) << std::endl;
 }
 int main() {
-	//correctionTest();
+	std::srand(std::time(0));
+	std::cout << "Output in format of:\nSet A:\nSet B:\nSet C:\nSet D:\nSet E:\n";
+	correctionTest();
 	std::cout << "All results are in microseconds" << std::endl;
-	testB();
-	testUS();
-	testC();
-	testL();
+	for (int i = 2; i <= 16; i += 2) {
+		std::cout << "Cardinality: " << std::dec << i << std::endl;
+		testB(i);
+		testUS(i);
+		testC(i);
+		testL(i);
+		std::cout << std::endl;
+	}
+
 	return 0;
 }
