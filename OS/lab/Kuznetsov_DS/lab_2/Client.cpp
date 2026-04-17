2d86ec19560bcf831e5e52f39ea0ee56ced22d42
Dunisiy <denis111.kuznetsov@yandex.ru>
2026-04-17 04:37:00 +0300

    init

--- OS/lab/Kuznetsov_DS/lab_2/Client.cpp
+++ OS/lab/Kuznetsov_DS/lab_2/Client.cpp
@@ -1,21 +1,20 @@
 #include <fcntl.h>
-#include <semaphore.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/mman.h>
+#include <sys/select.h>
 #include <sys/stat.h>
 #include <unistd.h>
 
 #define FILENAME "shared_memory.dat"
+#define PIPE_NAME "sync_pipe"
 #define FILESIZE 4096
 
 int main() {
 	int fd = -1;
-	char* base_ptr = NULL;
-	sem_t* ready_sem = NULL;
-	sem_t* ack_sem = NULL;
-	char* data_area = NULL;
+	int pipe_fd = -1;
+	char* ptr = NULL;
 	int choice;
 
 	while (1) {
@@ -25,71 +24,63 @@ int main() {
 		printf("3. Завершить работу\n");
 		printf("Введите выбор: ");
 
-		if (scanf("%d", &choice) != 1) {
-			while (getchar() != '\n');
-			continue;
-		}
+		scanf("%d", &choice);
 
 		switch (choice) {
-			case 1: {
-					if (fd != -1) {
-						printf("Проецирование уже выполнено!\n");
-						break;
-					}
-
-					fd = open(FILENAME, O_RDWR);
-					if (fd == -1) {
-						perror("Ошибка открытия файла");
-						break;
-					}
-
-					base_ptr = mmap(NULL, FILESIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
-					if (base_ptr == MAP_FAILED) {
-						perror("mmap");
-						close(fd);
-						fd = -1;
-						base_ptr = NULL;
-						break;
-					}
-
-					ready_sem = (sem_t*)base_ptr;
-					ack_sem = (sem_t*)(base_ptr + sizeof(sem_t));
-					data_area = base_ptr + 2 * sizeof(sem_t);
-
-					printf("Файл успешно проецирован в память (клиент).\n");
+			case 1:
+				if (fd != -1) {
+					printf("Уже выполнено!\n");
 					break;
 				}
 
-			case 2: {
-					if (base_ptr == NULL) {
-						printf("Сначала выполните проецирование (пункт 1)!\n");
-						break;
-					}
-
-					printf("Ожидаем данные от сервера...\n");
-
-					sem_wait(ready_sem);
-
-					printf("Client received: %s\n", data_area);
-
-					sem_post(ack_sem);
+				fd = open(FILENAME, O_RDWR);
+				if (fd == -1) {
+					perror("open файл");
 					break;
 				}
 
-			case 3: {
-					if (base_ptr != NULL) {
-						munmap(base_ptr, FILESIZE);
-					}
-					if (fd != -1) {
-						close(fd);
-					}
-					printf("Проецирование отменено. Работа клиента завершена.\n");
-					exit(0);
+				ptr = mmap(NULL, FILESIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+				if (ptr == MAP_FAILED) {
+					perror("mmap");
+					exit(1);
 				}
 
-			default:
-				printf("Неверный выбор! Попробуйте снова.\n");
+				pipe_fd = open(PIPE_NAME, O_RDONLY);
+				if (pipe_fd == -1) {
+					perror("open pipe");
+					exit(1);
+				}
+
+				printf("Клиент готов.\n");
+				break;
+
+			case 2:
+				if (ptr == NULL) {
+					printf("Сначала проецирование!\n");
+					break;
+				}
+
+				fd_set readfds;
+				FD_ZERO(&readfds);
+				FD_SET(pipe_fd, &readfds);
+
+				printf("Ожидание данных...\n");
+
+				select(pipe_fd + 1, &readfds, NULL, NULL, NULL);
+
+				char signal;
+				read(pipe_fd, &signal, 1);
+
+				printf("Получено: %s\n", ptr);
+				break;
+
+			case 3:
+				if (ptr) munmap(ptr, FILESIZE);
+				if (fd != -1) close(fd);
+				if (pipe_fd != -1) close(pipe_fd);
+
+				printf("Клиент завершён.\n");
+				exit(0);
 		}
 	}
-	return 0;
 }
--- OS/lab/Kuznetsov_DS/lab_2/Server.cpp
+++ OS/lab/Kuznetsov_DS/lab_2/Server.cpp
@@ -1,5 +1,4 @@
 #include <fcntl.h>
-#include <semaphore.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -8,15 +7,15 @@
 #include <unistd.h>
 
 #define FILENAME "shared_memory.dat"
+#define PIPE_NAME "sync_pipe"
 #define FILESIZE 4096
 
 int main() {
 	int fd = -1;
-	char* base_ptr = NULL;
-	sem_t* ready_sem = NULL;      // сервер сигнализирует «данные готовы»
-	sem_t* ack_sem = NULL;      // клиент сигнализирует «прочитал»
-	char* data_area = NULL;
+	int pipe_fd = -1;
+	char* ptr = NULL;
 	int choice;
+	char buffer[1024];
 
 	while (1) {
 		printf("\n=== МЕНЮ СЕРВЕРА ===\n");
@@ -25,92 +24,70 @@ int main() {
 		printf("3. Завершить работу\n");
 		printf("Введите выбор: ");
 
-		if (scanf("%d", &choice) != 1) {
-			while (getchar() != '\n');
-			continue;
-		}
+		scanf("%d", &choice);
+		getchar();
 
 		switch (choice) {
-			case 1: {
-					if (fd != -1) {
-						printf("Проецирование уже выполнено!\n");
-						break;
-					}
-
-					fd = open(FILENAME, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-					if (fd == -1) {
-						perror("Ошибка create/open");
-						break;
-					}
-
-					if (ftruncate(fd, FILESIZE) == -1) {
-						perror("ftruncate");
-						close(fd);
-						fd = -1;
-						break;
-					}
-
-					base_ptr = mmap(NULL, FILESIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
-					if (base_ptr == MAP_FAILED) {
-						perror("mmap");
-						close(fd);
-						fd = -1;
-						base_ptr = NULL;
-						break;
-					}
-
-					// Размещаем семафоры в начале отображённой памяти (без структуры)
-					ready_sem = (sem_t*)base_ptr;
-					ack_sem = (sem_t*)(base_ptr + sizeof(sem_t));
-					data_area = base_ptr + 2 * sizeof(sem_t);
-
-					// Инициализация один раз
-					sem_init(ready_sem, 1, 0);
-					sem_init(ack_sem, 1, 0);
-
-					printf("Файл успешно проецирован в память (сервер).\n");
+			case 1:
+				if (fd != -1) {
+					printf("Уже выполнено!\n");
 					break;
 				}
 
-			case 2: {
-					if (base_ptr == NULL) {
-						printf("Сначала выполните проецирование (пункт 1)!\n");
-						break;
-					}
+				// создаём pipe (FIFO)
+				mkfifo(PIPE_NAME, 0666);
 
-					// Жёстко заданные данные (как в исходной версии)
-					sprintf(data_area, "Hello, shared memory!");
-
-					// Сигнал клиенту
-					sem_post(ready_sem);
-
-					printf("Данные записаны в общую память.\n");
-					printf("Ожидаем подтверждения от клиента...\n");
-
-					// Ждём, пока клиент прочитает
-					sem_wait(ack_sem);
-					printf("Клиент успешно прочитал данные.\n");
+				fd = open(FILENAME, O_RDWR | O_CREAT | O_TRUNC, 0666);
+				if (fd == -1) {
+					perror("open");
 					break;
 				}
 
-			case 3: {
-					if (base_ptr != NULL) {
-						sem_destroy(ready_sem);
-						sem_destroy(ack_sem);
-						munmap(base_ptr, FILESIZE);
-					}
-					if (fd != -1) {
-						close(fd);
-					}
-					unlink(FILENAME);
+				ftruncate(fd, FILESIZE);
 
-					printf("Проецирование отменено, файл удалён. Работа сервера завершена.\n");
-					exit(0);
+				ptr = mmap(NULL, FILESIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+				if (ptr == MAP_FAILED) {
+					perror("mmap");
+					exit(1);
 				}
 
-			default:
-				printf("Неверный выбор! Попробуйте снова.\n");
+				pipe_fd = open(PIPE_NAME, O_WRONLY);
+				if (pipe_fd == -1) {
+					perror("pipe open");
+					exit(1);
+				}
+
+				printf("Сервер готов.\n");
+				break;
+
+			case 2:
+				if (ptr == NULL) {
+					printf("Сначала проецирование!\n");
+					break;
+				}
+
+				printf("Введите строку: ");
+				fgets(buffer, sizeof(buffer), stdin);
+				buffer[strcspn(buffer, "\n")] = '\0';
+
+				memset(ptr, 0, FILESIZE);
+				snprintf(ptr, FILESIZE, "%s", buffer);
+
+				write(pipe_fd, "1", 1);
+
+				printf("Данные записаны и сигнал отправлен.\n");
+				break;
+
+			case 3:
+				if (ptr) munmap(ptr, FILESIZE);
+				if (fd != -1) close(fd);
+				if (pipe_fd != -1) close(pipe_fd);
+
+				unlink(FILENAME);
+				unlink(PIPE_NAME);
+
+				printf("Сервер завершён.\n");
+				exit(0);
 		}
 	}
-	return 0;
 }
